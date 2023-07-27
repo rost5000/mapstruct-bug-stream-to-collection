a1fc7bb9e050abc7b33edc6d2684d24543a0590a
rost5000 <rshanyiazov@vtb.ru>
2023-07-27 14:01:56 +0400

    Modify usecases

--- src/main/java/org/example/SimpleMapper.java
+++ src/main/java/org/example/SimpleMapper.java
@@ -2,13 +2,24 @@ package org.example;
 
 import org.example.domain.SimpleModel;
 import org.example.dto.SimpleModelResponse;
+import org.example.dto.SimpleModelResponse1;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.stream.Stream;
 
+@SuppressWarnings("unused")
 @Mapper
 public interface SimpleMapper {
+    /**
+     * This method will be not compiled
+     */
     @Mapping(target = "model")
     SimpleModelResponse toDto(Stream<SimpleModel> model);
+
+    /**
+     * This method will be compiled
+     */
+    @Mapping(target = "model")
+    SimpleModelResponse1 toDto(SimpleModel model);
 }
---
+++ src/main/java/org/example/dto/SimpleModelResponse1.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+import org.example.domain.SimpleModel;
+
+import java.util.List;
+
+public class SimpleModelResponse1 {
+    SimpleModelDto model;
+
+    public SimpleModelDto getModel() {
+        return model;
+    }
+
+    public void setModel(SimpleModelDto model) {
+        this.model = model;
+    }
+}
